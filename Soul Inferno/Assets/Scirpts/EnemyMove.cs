b69ec42f8ae5f3816d00be775fc97beec1e8e756
sopli2 <sli2@stu.naperville203.org>
2021-06-03 14:03:07 -0500

    update

---
+++ Soul Inferno/Assets/Scirpts/EnemyMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+
+    // Array of waypoints to walk from one to the next one
+    [SerializeField]
+    private Transform[] waypoints;
+
+    // Walk speed that can be set in Inspector
+    [SerializeField]
+    private float moveSpeed = 2f;
+
+    private int waypointIndex = 0;
+
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.flipX = true;
+    }
+
+    private void Update()
+    {
+        // Move Enemy
+        Move();
+
+    }
+
+    // Method that actually make Enemy walk
+    private void Move()
+    {
+        // If Enemy didn't reach last waypoint it can move
+        // If enemy reached last waypoint then it stops
+
+            // Move Enemy from current waypoint to the next one
+            // using MoveTowards method
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
+
+            // If Enemy reaches position of waypoint he walked towards
+            // then waypointIndex is increased by 1
+            // and Enemy starts to walk to the next waypoint
+        
+        if (transform.position == waypoints[waypointIndex].transform.position)
+        {
+            waypointIndex += 1;
+            spriteRenderer.flipX = false;
+        }
+        if (waypointIndex == waypoints.Length)
+        {
+            waypointIndex = 0;
+            spriteRenderer.flipX = true;
+        }
+    }
+}
---
+++ Soul Inferno/Assets/Scirpts/Move.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    //reference to the animator component
+    Animator animator;
+    Rigidbody2D rb2d;
+    SpriteRenderer spriteRenderer;
+    AudioSource audioWalk;
+    AudioSource audioJump;
+
+    bool isGrounded;
+
+    [SerializeField]
+    Transform groundCheck = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get the animator component of the GameObject
+        animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        AudioSource[] allMyAudioSources = GetComponents<AudioSource>();
+        audioWalk = allMyAudioSources[0];
+        audioJump = allMyAudioSources[1];
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground")))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        // change the animator's AnimState variable if a key is pressed
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            if (!isGrounded)
+            {
+                animator.SetInteger("AnimState", 2);
+                audioWalk.Stop();
+            }
+            else
+            {
+                if (audioWalk.isPlaying == false)
+                {
+                    audioWalk.Play();
+                }
+                animator.SetInteger("AnimState", 1);
+            }
+            rb2d.velocity = new Vector2(2, rb2d.velocity.y);
+            spriteRenderer.flipX = false;
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            if (!isGrounded)
+            {
+                animator.SetInteger("AnimState", 2);
+                audioWalk.Stop();
+            }
+            else
+            {
+                if (audioWalk.isPlaying == false)
+                {
+                    audioWalk.Play();
+                }
+                animator.SetInteger("AnimState", 1);
+            }
+            rb2d.velocity = new Vector2(-2, rb2d.velocity.y);
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            animator.SetInteger("AnimState", 0);
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+            audioWalk.Stop();
+        }
+        if (Input.GetKey("up") && isGrounded || Input.GetKey("w") && isGrounded)
+        {
+            audioJump.Play();
+            animator.SetInteger("AnimState", 2);
+            rb2d.velocity = new Vector2(rb2d.velocity.x, 5);
+        }
+    }
+}
