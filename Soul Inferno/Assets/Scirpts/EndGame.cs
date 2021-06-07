ff1e2e6c57b6c310a0602929452f7ba4458134d8
sopli2 <sli2@stu.naperville203.org>
2021-06-07 13:24:30 -0500

    Final

---
+++ Soul Inferno/Assets/Scirpts/EndGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    [SerializeField]
+    private string nextSceneName;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            SceneManager.LoadScene(nextSceneName);
+    }
+}
--- Soul Inferno/Assets/Scirpts/LevelManager.cs
+++ Soul Inferno/Assets/Scirpts/LevelManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
@@ -8,6 +9,17 @@ public class LevelManager : MonoBehaviour
 
     public Transform respawnPoint;
     public GameObject playerPrefab;
+
+    public void LoadLevel(string level)
+    {
+        Debug.Log("Attempting to load " + level);
+        SceneManager.LoadScene(level);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Attempting to quit... ");
+        Application.Quit();
+    }
 
     private void Awake()
     {
