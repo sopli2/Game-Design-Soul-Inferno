8e993654fd838304e023508f6d8278ed6788f508
sopli2 <sli2@stu.naperville203.org>
2021-06-07 10:41:35 -0500

    Update (Mon)

---
+++ Soul Inferno/Assets/Scirpts/PlayerDeath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+
+	private bool hasEntered;
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == ("Death") && !hasEntered)
+		{
+			hasEntered = true;
+			Destroy(gameObject);
+			LevelManager.instance.Respawn();
+		}
+	}
+}
